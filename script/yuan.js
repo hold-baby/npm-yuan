240f1556665d9b694be5be014af5e838023d161e
袁昌瑞 <hold_baby@qq.com>
2020-08-07 16:20:26 +0800

    add huawei

--- script/yuan.js
+++ script/yuan.js
@@ -1,4 +1,9 @@
 module.exports = {
 	"taobao" : "https://registry.npm.taobao.org/",
-	"npm" : "https://registry.npmjs.org/"
+	"npm" : "https://registry.npmjs.org/",
+	"huawei" : "https://mirrors.huaweicloud.com/repository/npm/",
+
+	"tb" : "https://registry.npm.taobao.org/",
+	"npm" : "https://registry.npmjs.org/",
+	"hw" : "https://mirrors.huaweicloud.com/repository/npm/"
 }
